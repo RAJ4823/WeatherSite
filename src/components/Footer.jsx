be0cc860db3f0edd090d820e137b243b4e24855c
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2023-08-02 10:47:21 +0530

    Footer text change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
       <p className="footer-title">
-        Made by{" "}
-        <a className="accent-text" href="https://raj4823.github.io/MySite">
-          Raj Patel
-        </a>
+        <span>© Superverse {" "}</span>
+        <span>{currentYear}</span>
+        <span> | </span>
+        <a className="accent-text" href="https://raj4823.github.io/MySite" target="_blank">Raj Patel</a>
       </p>
     </div>
   );
